409b86bb7925e22ee9d053a419fea3e94f5b842b
Meatssauce <70827089+Meatssauce@users.noreply.github.com>
2022-11-16 11:36:52 +1100

    implemented Block, Transaction, Input and Output, as well as draft ledger building logic

---
+++ Block.py
@@ -0,0 +1,177 @@
+import re
+from dataclasses import dataclass
+from typing import IO
+
+from opcodes import Opcode
+
+
+def read_varint(file: IO) -> int:
+    tx_count = file.read(1).hex()
+
+    if tx_count.startswith('fd'):
+        tx_count = file.read(2)[::-1].hex()
+    elif tx_count.startswith('fe'):
+        tx_count = file.read(4)[::-1].hex()
+    elif tx_count.startswith('ff'):
+        tx_count = file.read(8)[::-1].hex()
+
+    return int(tx_count, base=16)
+
+
+# def read_varint_bytes(stream: bytes) -> tuple[int, int]:
+#     tx_count = file.read(1).hex()
+#     total_bytes_read = 1
+#
+#     if tx_count.startswith('fd'):
+#         tx_count = file.read(2)[::-1].hex()
+#         total_bytes_read += 2
+#     elif tx_count.startswith('fe'):
+#         tx_count = file.read(4)[::-1].hex()
+#         total_bytes_read += 4
+#     elif tx_count.startswith('ff'):
+#         tx_count = file.read(8)[::-1].hex()
+#         total_bytes_read += 8
+#
+#     return int(tx_count, base=16), total_bytes_read
+
+
+@dataclass(frozen=True)
+class Input:
+    tx_id: str  # that of a tx whose output we take as input
+    vout: str  # index of input as an output in the tx
+    scriptSig_size: int  # number of bytes
+    scriptSig: str
+    sequence: str  # ignored
+
+    @classmethod
+    def from_file(cls, file: IO):
+        tx_id = file.read(32)[::-1].hex()
+        vout = file.read(4)[::-1].hex()
+        scriptSig_size = read_varint(file)
+        scriptSig = file.read(scriptSig_size).hex()
+        sequence = file.read(4)[::-1].hex()
+        return cls(tx_id, vout, scriptSig_size, scriptSig, sequence)
+
+    # @classmethod
+    # def from_bytes(cls, stream: bytes) -> 'Input':
+    #     tx_id = stream[:32][::-1].hex()
+    #     stream = stream[32:]
+    #
+    #     vout = stream[:4][::-1].hex()
+    #     stream = stream[4:]
+    #
+    #     scriptSig_size, bytes_read = read_varint_bytes(stream)
+    #     stream = stream[bytes_read:]
+    #
+    #     scriptSig = stream[:scriptSig_size].hex()
+    #     stream = stream[scriptSig_size:]
+    #
+    #     sequence = stream[:4][::-1].hex()
+    #     stream = stream[4:]
+    #
+    #     return cls(tx_id, vout, scriptSig_size, scriptSig, sequence)
+
+    def is_coinbase(self):
+        return self.tx_id == '0' * 64 and self.vout == 'f' * 8
+
+
+@dataclass(frozen=True)
+class Output:
+    value: int  # amount of BTC in satoshis.
+    scriptPubKey_size: int  # number of bytes
+    scriptPubKey: str
+
+    @classmethod
+    def from_file(cls, file: IO):
+        value = int(file.read(8)[::-1].hex(), base=16)
+        scriptPubKey_size = read_varint(file)
+        scriptPubKey = file.read(scriptPubKey_size).hex()
+        return cls(value, scriptPubKey_size, scriptPubKey)
+
+
+    @property
+    def recipients(self):
+        """Bitcoin address(es) or public key(s) to which the output instance is sent"""
+
+        # scriptPubKey patterns
+        p2pkh = f'{Opcode.DUP}{Opcode.HASH160}([0-9a-f]{{40}}){Opcode.EQUALVERFIY}{Opcode.CHECKSIG}'
+        p2pk = f'{Opcode.ONE}([0-9a-f]{{130}}){Opcode.THREE}{Opcode.CHECKMULTISIG}'
+        p2ms = f'{Opcode.HASH160}([0-9a-f]{{130}}){{1,3}}{Opcode.EQUAL}'
+        p2sh = f'{Opcode.RETURN}([0-9a-f]{{40}})'
+        # null_data = f'([0-9a-f]+){Opcode.CHECKSIG}'
+
+        if key_search := re.search(p2ms, self.scriptPubKey):
+            return list(key_search.groups())
+
+        for pattern in [p2pkh, p2pk, p2sh]:
+            if key_search := re.search(pattern, self.scriptPubKey):
+                return [key_search.group(1)]
+
+        return []
+
+
+@dataclass(frozen=True)
+class Transaction:
+    version: str
+    input_count: int
+    inputs: [Input]
+    output_count: int
+    outputs: [Output]
+    locktime: str
+
+    @classmethod
+    def from_file(cls, file: IO):
+        version = file.read(4)[::-1].hex()
+
+        input_count = read_varint(file)
+        inputs = [Input.from_file(file) for _ in range(input_count)]
+
+        output_count = read_varint(file)
+        outputs = [Output.from_file(file) for _ in range(output_count)]
+
+        locktime = file.read(4)[::-1].hex()
+
+        return cls(version, input_count, inputs, output_count, outputs, locktime)
+
+    def is_coinbase(self):
+        return len(self.inputs) == 1 and self.inputs[0].tx_id == '0' * 64 and self.inputs[0].vout == 'f' * 8
+
+
+@dataclass(frozen=True)
+class Block:
+    magic_bytes: str
+    size: int  # number of bytes
+
+    # block_header = f.read(80).hex()
+
+    version: str
+    prev_block_hash: str
+    merkle_root: str
+    time_: str
+    bits: str
+    nonce: str
+
+    tx_count: int
+    transactions: [Transaction]
+
+    @classmethod
+    def from_file(cls, file: IO):
+        magic_bytes = file.read(4)[::-1].hex()
+        size = int(file.read(4)[::-1].hex(), base=16)
+
+        # block_header = f.read(80).hex()
+
+        version = file.read(4)[::-1].hex()
+        prev_block_hash = file.read(32)[::-1].hex()
+        merkle_root = file.read(32)[::-1].hex()
+        time_ = file.read(4)[::-1].hex()
+        bits = file.read(4)[::-1].hex()
+        nonce = file.read(4)[::-1].hex()
+
+        tx_count = read_varint(file)
+        transactions = [Transaction.from_file(file) for _ in range(tx_count)]
+
+        return cls(magic_bytes, size,
+                   version, prev_block_hash, merkle_root, time_, bits, nonce,
+                   tx_count, transactions)
+
--- block_parser.py
+++ block_parser.py
@@ -1,7 +1,16 @@
+import glob
 import hashlib
+import os
+import re
 import warnings
+from collections import defaultdict
 from itertools import pairwise
 from typing import IO
+
+from tqdm import tqdm
+
+from Block import Block, Output
+from opcodes import Opcode
 
 
 def make_merkle_root(lst):  # https://gist.github.com/anonymous/7eb080a67398f648c1709e41890f8c44
@@ -17,97 +26,134 @@ def make_merkle_root(lst):  # https://gist.github.com/anonymous/7eb080a67398f648
     return make_merkle_root([hash_pair(x, y) for x, y in pairwise(lst)])
 
 
-def read_varint(file: IO) -> int:
-    tx_count = file.read(1).hex()
-
-    if tx_count.startswith('fd'):
-        tx_count = file.read(2)[::-1].hex()
-    elif tx_count.startswith('fe'):
-        tx_count = file.read(4)[::-1].hex()
-    elif tx_count.startswith('ff'):
-        tx_count = file.read(8)[::-1].hex()
-
-    return int(tx_count, base=16)
-
-
-def read_tx_input(file: IO) -> dict:
-    tx_id = file.read(32)[::-1].hex()
-    vout = file.read(4)[::-1].hex()
-    scriptSig_size = read_varint(file)
-    scriptSig = file.read(scriptSig_size).hex()
-    sequence = file.read(4)[::-1].hex()
-
-    return {'tx_id': tx_id, 'vout': vout, 'scriptSig': scriptSig, 'sequence': sequence}
-
-
-def read_tx_output(file: IO) -> dict:
-    value = file.read(8)[::-1].hex()
-    scriptPubKey_size = read_varint(file)
-    scriptPubKey = file.read(scriptPubKey_size).hex()
-
-    return {'value': value, 'scriptPubKey': scriptPubKey}
-
-
-def read_transaction(file: IO) -> dict:
-    version = file.read(4)[::-1].hex()
-
-    input_count = read_varint(file)
-    inputs = [read_tx_input(file) for _ in range(input_count)]
-
-    output_count = read_varint(file)
-    outputs = [read_tx_output(file) for _ in range(output_count)]
-
-    locktime = file.read(4)[::-1].hex()
-
-    return {'version': version, 'inputs': inputs, 'outputs': outputs, 'locktime': locktime}
-
-
-# def read_block(block_dir):
-#     block_dir = 'D:/Bitcoin/blocks/'  # Directory where blk*.dat files are stored
-#     out_dir = './result/'  # Directory where to save parsing results
+# def read_varint(file: IO) -> int:
+#     tx_count = file.read(1).hex()
 #
-#     for filename in sorted(x for x in os.listdir(block_dir) if x.endswith('.dat') and x.startswith('blk')):
-#         filepath = block_dir + filename
+#     if tx_count.startswith('fd'):
+#         tx_count = file.read(2)[::-1].hex()
+#     elif tx_count.startswith('fe'):
+#         tx_count = file.read(4)[::-1].hex()
+#     elif tx_count.startswith('ff'):
+#         tx_count = file.read(8)[::-1].hex()
 #
-#         msg = f'Reading block from {filepath} at {datetime.datetime.now()}\n'
-#         print(msg)
+#     return int(tx_count, base=16)
+#
+#
+# def read_tx_input(file: IO) -> dict:
+#     tx_id = file.read(32)[::-1].hex()
+#     vout = file.read(4)[::-1].hex()
+#     scriptSig_size = read_varint(file)
+#     scriptSig = file.read(scriptSig_size).hex()
+#     sequence = file.read(4)[::-1].hex()
+#
+#     return {'tx_id': tx_id, 'vout': vout, 'scriptSig': scriptSig, 'sequence': sequence}
+#
+#
+# def read_tx_output(file: IO) -> dict:
+#     value = file.read(8)[::-1].hex()
+#     scriptPubKey_size = read_varint(file)
+#     scriptPubKey = file.read(scriptPubKey_size).hex()
+#
+#     return {'value': value, 'scriptPubKey': scriptPubKey}
+#
+#
+# def read_transaction(file: IO) -> dict:
+#     version = file.read(4)[::-1].hex()
+#
+#     input_count = read_varint(file)
+#     inputs = [read_tx_input(file) for _ in range(input_count)]
+#
+#     output_count = read_varint(file)
+#     outputs = [read_tx_output(file) for _ in range(output_count)]
+#
+#     locktime = file.read(4)[::-1].hex()
+#
+#     return {'version': version, 'inputs': inputs, 'outputs': outputs, 'locktime': locktime}
+#
+#
+# # def read_block(block_dir):
+# #     block_dir = 'D:/Bitcoin/blocks/'  # Directory where blk*.dat files are stored
+# #     out_dir = './result/'  # Directory where to save parsing results
+# #
+# #     for filename in sorted(x for x in os.listdir(block_dir) if x.endswith('.dat') and x.startswith('blk')):
+# #         filepath = block_dir + filename
+# #
+# #         msg = f'Reading block from {filepath} at {datetime.datetime.now()}\n'
+# #         print(msg)
+#
+#
+# def read_block(file: IO) -> dict:
+#     magic_bytes = file.read(4)[::-1].hex()
+#     size = file.read(4)[::-1].hex()
+#
+#     # block_header = f.read(80).hex()
+#
+#     version = file.read(4)[::-1].hex()
+#     prev_block_hash = file.read(32)[::-1].hex()
+#     merkle_root = file.read(32)[::-1].hex()
+#     time_ = file.read(4)[::-1].hex()
+#     bits = file.read(4)[::-1].hex()
+#     nonce = file.read(4)[::-1].hex()
+#
+#     tx_count = read_varint(file)
+#     transactions = [read_transaction(file) for _ in range(tx_count)]
+#
+#     # if make_merkle_root(...) != merkle_root:
+#     #     warnings.warn('Merkle root does not match transactions. '
+#     #                   'Something is wrong if this is an official block.')
+#
+#     return {'magic_bytes': magic_bytes, 'size': size, 'version': version, 'prev_block_hash': prev_block_hash,
+#             'merkle_root': merkle_root, 'time': time_, 'bits': bits, 'nonce': nonce, 'transactions': transactions}
 
 
-def read_block(file: IO) -> dict:
-    magic_bytes = file.read(4)[::-1].hex()
-    size = file.read(4)[::-1].hex()
-
-    # block_header = f.read(80).hex()
-
-    version = file.read(4)[::-1].hex()
-    prev_block_hash = file.read(32)[::-1].hex()
-    merkle_root = file.read(32)[::-1].hex()
-    time_ = file.read(4)[::-1].hex()
-    bits = file.read(4)[::-1].hex()
-    nonce = file.read(4)[::-1].hex()
-
-    tx_count = read_varint(file)
-    transactions = [read_transaction(file) for _ in range(tx_count)]
-
-    # if make_merkle_root(...) != merkle_root:
-    #     warnings.warn('Merkle root does not match transactions. '
-    #                   'Something is wrong if this is an official block.')
-
-    return {'magic_bytes': magic_bytes, 'size': size, 'version': version, 'prev_block_hash': prev_block_hash,
-            'merkle_root': merkle_root, 'time': time_, 'bits': bits, 'nonce': nonce, 'transactions': transactions}
-
-
-def read_dat(filepath) -> [dict]:
-    blocks = []
-
+def read_dat(filepath: str):
     with open(filepath, 'rb') as f:
         while True:
             try:
-                blocks.append(read_block(f))
+                yield Block.from_file(f)
             except EOFError:
                 break
 
-    return blocks
+
+def update_ledger(block: Block, past_tx: dict[str, list], utxo: defaultdict[any, set], balances: dict[str, float]):
+    for tx in block.transactions:
+        for input_ in tx.inputs:
+            if input_.is_coinbase():
+                continue
+
+            txo_being_spent = past_tx[input_.tx_id][input_.vout - 1]
+
+            for sender in txo_being_spent.recipients:
+                utxo[sender].remove(txo_being_spent)
+                balances[sender] -= txo_being_spent.value
+
+        for output in tx.outputs:
+            for recipient in output.recipients:
+                utxo[recipient].add(output)
+                balances[recipient] += output.value
+
+    return utxo, balances
+
+
+def build_ledger_history(blocks_dir: str, end: int):
+    """Build history of account balances from Bitcoin blocks"""
+
+    past_tx = {}
+    utxo = defaultdict(set)
+    balances = {}
+    block_height = 0
+
+    for filename in tqdm(glob.glob(os.path.join(blocks_dir, 'blk*.dat'))):
+        filepath = os.path.join(blocks_dir, filename)
+
+        for block in read_dat(filepath):
+            if block_height >= end - 1:
+                return utxo, balances
+
+            utxo, balances = update_ledger(block, past_tx, utxo, balances)
+            block_height += 1
+
+    return utxo, balances
 
 
 def main():
